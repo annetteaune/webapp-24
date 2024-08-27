785a469fd33d916b158d3cd8470b3d1cb25eb1bb
annetteaune <annetteaune@outlook.com>
2024-08-27 18:19:25 +0200

    kan opprette nytt prosjekt med skjema

--- oblig1/src/main.ts
+++ oblig1/src/main.ts
@@ -28,6 +28,7 @@ function createProjectCard(project: {
         </article>
     `;
 }
+
 //legge til prosjektkort i tillegg til eksisterende html
 function showProjects() {
   const mainElement = document.querySelector("main");
@@ -36,4 +37,41 @@ function showProjects() {
   }
 }
 
-showProjects();
+// Håndtere skjema
+function addNewProject(event: Event) {
+  event.preventDefault();
+
+  const form = event.target as HTMLFormElement;
+  const formData = new FormData(form);
+
+  const newProject = {
+    id: projects.length + 1,
+    title: formData.get("title") as string,
+    description: formData.get("description") as string,
+    image: formData.get("imageLink") as string,
+    liveLink: formData.get("liveLink") as string,
+    codeLink: formData.get("codeLink") as string,
+  };
+
+  projects.push(newProject);
+
+  const mainElement = document.querySelector("main");
+  if (mainElement) {
+    mainElement.innerHTML += createProjectCard(newProject);
+  }
+
+  form.reset();
+}
+
+// Initialisere siden
+function init() {
+  showProjects();
+
+  // Lytt etter knappetykk
+  const form = document.getElementById("add-project-form");
+  if (form) {
+    form.addEventListener("submit", addNewProject);
+  }
+}
+
+init();
